9e55cab89d1794509435647471a35d319386d104
dhiraj dhungana <ddhungana@deerwalk.com>
2020-02-25 11:27:37 +0545

    second commit

--- grails-app/domain/test/Person.groovy
+++ grails-app/domain/test/Person.groovy
@@ -3,10 +3,14 @@ package test
 class Person {
 
     String name;
-    String age;
+    int age;
     String email;
     String password;
 
     static constraints = {
+        name(blank: true,nullable:false)
+        age(blank: true,nullable:false)
+        name(email: true,nullable:true)
+        name(password: true,nullable:false)
     }
 }
